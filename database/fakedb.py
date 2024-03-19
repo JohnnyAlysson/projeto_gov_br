74d574ab61dffb470dd330047ad4fa908fde23d7
JohnnyAlysson <149841185+JohnnyAlysson@users.noreply.github.com>
2024-03-18 22:28:11 -0300

    Try another code

--- database/fakedb.py
+++
@@ -1,8 +0,0 @@
-CLIENTS = [
-    {"id":1, "nome":"Fulano", "email": "example@test.com"},
-    {"id":2, "nome":"Xiquim", "email": "teste@test.com"},
-    {"id":3, "nome":"Fernanda", "email": "oi@test.com"},
-    {"id":4, "nome":"Sicrana", "email": "firula@test.com"},
-    {"id":5, "nome":"Duck", "email": "hadouken@test.com"}
-
-]
