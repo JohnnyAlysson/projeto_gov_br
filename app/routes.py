4d2e5493fc82dfeb5ba599f1e9d80caf19e9ec29
JohnnyAlysson <149841185+JohnnyAlysson@users.noreply.github.com>
2024-03-19 09:30:14 -0300

    first set up

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
 from flask import render_template
 from app import app
 
+# Rotas clientes:
+    
+#     - /clients/ (GET)                   -  Listar clientes
+#     - /clients/ (POST)                  -  Inserir cliente no servidor 
+#     - /clients/new (GET)                - renderizar o formulário para criar um cliente
+#     - /clients/<id> (GET)               - obter dados de um cliente
+#     - /clients/<id>/edit  (GET)         - renderizar o formulário para editar um cliente
+#     - /clients/<id>/update (PUT)        - atualizar os dados do cliente
+#     - /clients/<id>/delete (DELETE)     - deletar os dados do cliente
+
+#     obs: Rotas sem "methods =" são por padrão methods =  "GET"
+
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    dados = {"nome": "fulano","idade": 28, "job": "Customer suport",}
+
+    nome = dados.get("nome")
+    idade = dados.get("idade")
+    job = dados.get("job")
+    return render_template("index.html", nome = nome, idade = idade, job=job)
